20b0484c4be2c48edf05e0d0b1e1c885d2a0cfc8
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-10 23:09:11 -0700

    xcasa

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,7 +9,7 @@
         <form>
             <input type="text" name="username" id="username">
             <input type="password" name="password" id="password">
-            <button name="login-button" id="login-button">
+            <button name="login-button" class="login-button">
                 <span>
                     Login
                 </span>
