5b85b874b562ab523dcf62790d65fba47619495f
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-10 20:33:23 -0700

    test

---
+++ resources/views/login.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Blizrd</title>
+
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    </head>
+    <body>
+       <div>HELLO WORLD!!</div>
+    </body>
+</html>
