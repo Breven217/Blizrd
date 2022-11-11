dccce9948d609edef8aa3b566d255e2510017eb7
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-10 21:57:42 -0700

    test

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@push('styles')
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+@endpush
+
 @section('content')
     <h3>{{$gamer}}</h1>
     <div>Log me in baby</div>
