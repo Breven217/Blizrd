440b2a27c438ae1d1c7ad2a036354d8aad486873
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-13 14:50:18 -0700

    test

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,7 @@
-class Login {
-    checkLogin(event) {
+
+function checkLogin(event) {
     event.preventDefault();
     console.log('hereIAm')
     let content = document.getElementsByClassName('content')[0]
     content.innerHTML = '<i class="fa-thin fa-snowflake fa-spin fa-4x"></i>'
-    }
 }
-
