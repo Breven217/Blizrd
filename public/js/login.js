abcbfc1965ea178337c7c8df042dfd4ceceba8e1
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-13 15:26:10 -0700

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,14 @@
 
 async function checkLogin(event) {
     event.preventDefault();
-console.log(event.target.username);
     let content = document.getElementsByClassName('content')[0]
     content.innerHTML = '<i class="fa-regular fa-snowflake fa-spin fa-4x"></i>'
 
     let response = await fetch("/login", {
         body: {
-            username : event.target.input.username,
-            password : event.target.input.password
+            username : event.target.username.value,
+            password : event.target.password.value
         }
     });
-
+    console.log(response);
 }
