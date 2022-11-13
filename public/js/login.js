a7bda784897b2ac6f824da54479caa351519f2c2
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-13 15:08:32 -0700

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,9 @@
 
 function checkLogin(event) {
     event.preventDefault();
-    
+
     let content = document.getElementsByClassName('content')[0]
     content.innerHTML = '<i class="fa-regular fa-snowflake fa-spin fa-4x"></i>'
+
+    fetch("blizrd.com/login");
 }
