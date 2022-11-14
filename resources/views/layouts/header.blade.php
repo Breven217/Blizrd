7add1af46da523126de9acca62f5c54b666f9d8c
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-14 14:56:30 -0700

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,1 +1,1 @@
-<iframe src="{{ asset('images/SnowFall.gif') }}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+<iframe src="{{ asset('images/SnowFall.gif') }}"></iframe>
