083a32a5e53d0014c5394c0382875fe36cd2dacf
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-14 14:52:36 -0700

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,1 +1,2 @@
 <h1>Header</h1>
+<div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/xkGkrAIbJqAtKlsZI5" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/stickers/GVCC-xkGkrAIbJqAtKlsZI5">via GIPHY</a></p>
