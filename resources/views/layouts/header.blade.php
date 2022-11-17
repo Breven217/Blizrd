6f8bc7421aa1cfdff503b11aa8a7f0c5b7f70731
Breven Joyner <breven.joyner@maloufcompanies.com>
2022-11-16 23:16:33 -0700

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,1 +1,7 @@
-
+<div class="navbar">
+    <div class="navbutton">Home</div>
+    <div class="navbutton">Reports</div>
+    <div class="navbutton">Invoicing</div>
+    <div class="navbutton">Management</div>
+    <div class="navbutton">Logout</div>
+</div>
