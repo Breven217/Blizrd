6c0e699d8c93460c4f7ce4f71f8548050eb4f092
Breven Joyner <breven.joyner@maloufcompanies.com>
2022-11-16 23:55:50 -0700

    test

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,14 +1,15 @@
 <div class="navbar">
     @php
         $url = '';
-        if(Request::path() == 'home'){
-            $url = "home";
-        }
+        if(Request::path() == 'home'){ $url = "home";}
+        if(Request::path() == 'reports'){ $url = "reports";}
+        if(Request::path() == 'invoicing'){ $url = "invoicing";}
+        if(Request::path() == 'management'){ $url = "management";}
     @endphp
 
     <div @class(['navbutton', 'navbutton-active' => ($url == "home")])>Home</div>
-    <div class="navbutton">Reports</div>
-    <div class="navbutton">Invoicing</div>
-    <div class="navbutton">Management</div>
+    <div @class(['navbutton', 'navbutton-active' => ($url == "reports")])>Reports</div>
+    <div @class(['navbutton', 'navbutton-active' => ($url == "invoicing")])>Invoicing</div>
+    <div @class(['navbutton', 'navbutton-active' => ($url == "management")])>Management</div>
     <div class="navbutton">Logout</div>
 </div>
