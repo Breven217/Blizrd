41b0f26fe4b028e701d1b26c34191aee99e09a09
Breven Joyner <breven.joyner@maloufcompanies.com>
2022-11-16 22:12:06 -0700

    test

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,10 +12,9 @@
     <div class="content">
         <form onsubmit="checkLogin(event)" method="POST">
             @csrf
-            <div class="tooltip">Hover over me
+            <input type="text" name="username" id="username" class="tooltip">   
                 <span class="tooltiptext">Tooltip text</span>
-            </div>
-            <input type="text" name="username" id="username">
+            </input>
             <input type="password" name="password" id="password">
             <button name="login-button" class="login-button">
                 <span>
