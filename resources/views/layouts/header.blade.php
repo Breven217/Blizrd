38ffe3ec0b6f42cbe116abfa18af0259d3f07989
Breven Joyner <breven.joyner@maloufcompanies.com>
2022-11-16 23:40:50 -0700

    test

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,11 @@
 <div class="navbar">
-    <div class="navbutton">Home</div>
+    @php
+        if(Request::url() == 'blizrd.com/home'){
+            $home = true;
+        }
+    @endphp
+
+    <div @class(['navbutton', 'active' => $home])>Home</div>
     <div class="navbutton">Reports</div>
     <div class="navbutton">Invoicing</div>
     <div class="navbutton">Management</div>
