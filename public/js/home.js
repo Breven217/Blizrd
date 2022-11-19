23425f6d06e912a850832f0a3f7fe0fdd40e17c6
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-19 12:58:09 -0700

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,7 @@
 async function loadWeather(){
     let content = document.getElementsByClassName('content')[0]
     console.log('called');
-    await fetch("api.openweathermap.org/data/2.5/forecast?lat=41.68417&lon=-111.67957&appid=54a6d10f3bffb5d3e5e67b9fa58a63a6", {
+    await fetch("https://api.openweathermap.org/data/2.5/forecast?lat=41.68417&lon=-111.67957&appid=54a6d10f3bffb5d3e5e67b9fa58a63a6", {
         headers: {
             'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
         }
