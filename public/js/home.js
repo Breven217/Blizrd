62d67026c24a53e44878dc10bb04c26cf1365d03
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-19 10:13:54 -0700

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,17 @@
 async function loadWeather(){
     let content = document.getElementsByClassName('content')[0]
+
+    await fetch("https://api.weather.gov/points/41.7370,111.8338", {
+        headers: {
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.error){
+            //throw up an error modal here
+        }
+        else{
+        }
+    });
 }
