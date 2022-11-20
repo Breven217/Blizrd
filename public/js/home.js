0d2b23e6d3a9fdb45ab9382b3fb21ff2ec56a2b1
Breven Joyner <breven.joyner@maloufcompanies.com>
2022-11-20 01:36:05 -0700

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -21,14 +21,14 @@ async function loadWeather(){
             switch(data.weather[0].main) {
                 case 'Clear':
                     if (isDayTime){
-                        weatherIcon = '<i class="fa-solid fa-sun extra-large-font"></i>'
+                        weatherIcon = '<i class="fa-solid fa-sun fa-6x"></i>'
                     }
                     else {
-                        weatherIcon = '<i class="fa-solid fa-moon extra-large-font"></i>'
+                        weatherIcon = '<i class="fa-solid fa-moon fa-6x"></i>'
                     }
                   break;
                 default:
-                    weatherIcon = '<i class="fa-solid fa-temperature-quarter extra-large-font"></i>'
+                    weatherIcon = '<i class="fa-solid fa-temperature-quarter fa-6x"></i>'
               }
 
             container.className = "current-weather-populated"
