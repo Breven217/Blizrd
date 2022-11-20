1cc1bd034f1ae2cc5a9de15ed678a92d815e4964
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-19 21:16:05 -0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -70,6 +70,8 @@ class LoginController extends Controller
     }
 
     public function test(){
+        https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
         return file_get_contents("https://api.openweathermap.org/data/2.5/forecast?lat=41.68417&lon=-111.67957&appid=54a6d10f3bffb5d3e5e67b9fa58a63a6");
     }
 }
