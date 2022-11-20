053650e5f8e220efb41501216d9a18852e8345eb
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-20 13:18:30 -0700

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -27,7 +27,8 @@ async function forecast()
             for (let i = 0; i < data.length; i++){
                 newContent += '<div class="forecast-day">' + 
                     '<div>' + data[i].day + '</div>' +  
-                    '<div>' + data[i].temp_min + '°F-' + data[i].temp_max +'°F</div>' +
+                    '<div>' + 'Max: ' + data[i].temp_min + '°F<br>' +
+                    'Min: ' + data[i].temp_max +'°F</div>' +
                     '<div class="forecast-day-hour">'
 
                 for (y = 0; y < data[i].data.length; y++){
