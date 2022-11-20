65d76e2d228c3020eaa67d27294f74c8ccb291bb
Breven Joyner <breven.joyner@maloufcompanies.com>
2022-11-19 22:28:13 -0700

    Update WeatherController.php

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -8,7 +8,7 @@ class WeatherController extends Controller
 {
     public function getCurrentWeather(){
         return file_get_contents("https://api.openweathermap.org/data/2.5/weather?lat=" . 
-        config('app.logan_lat') . "&lon=" . config('app.logan_lon') . "&appid=" . config('app.openweather_key'));
+        config('app.logan_lat') . "&lon=" . config('app.logan_lon') . "&appid=" . config('app.openweather_key') . "&units=imperial");
 
         //return file_get_contents("https://api.openweathermap.org/data/2.5/forecast?lat=41.68417&lon=-111.67957&appid=54a6d10f3bffb5d3e5e67b9fa58a63a6");
     }
