205c62361af36cfebad7991214046f7777b594e1
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-23 15:46:21 -0700

    Update management.js

--- public/js/management.js
+++ public/js/management.js
@@ -37,9 +37,26 @@ console.log('here');
                     <th>Email Address</th>
                     <th>Receives Alert</th>
                 </tr>`
-            content.innerHTML = data;
+
+            data.forEach(result => {
+                let alertBox = '<i class="fa-regular fa-square"></i>'
+                if (result.receives_alerts){alertBox = '<i class="fa-regular fa-square-check"></i>'}
+                userTable += `
+                <tr>
+                    <td>` + result.name + `</td>
+                    <td>` + result.phone_number.slice(0,3) + '-' + 
+                        result.phone_number.slice(3,6) + '-' + 
+                        result.phone_number.slice(6,10) + 
+                    `</td>
+                    <td>` + result.email_address + `</td>
+                    <td>` + alertBox + `</td>
+                </tr>
+                `
+            });
 
             userTable += '</table>'
+
+            content.innerHTML = userTable
         }
     });
 }
