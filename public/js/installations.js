941634c7f72e34b8f59a82c1e529ffd394c1057a
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-24 10:28:12 -0700

    Update installations.js

--- public/js/installations.js
+++ public/js/installations.js
@@ -229,7 +229,7 @@ async function saveInstallation()
             actionData.set('installed',element.querySelector('#installed').value)
             actions.push(actionData)
         });
-        body.set('actions', actions.toString())
+        body.set('actions', JSON.stringify(actions))
     }
 
     content.innerHTML = '<div><i class="fa-regular fa-snowflake fa-spin fa-4x vertical-center"></i></div>'
