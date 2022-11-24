a8900b41f88de4933ef776cb1d1795c84c2e9164
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-23 19:53:15 -0700

    Update management.js

--- public/js/management.js
+++ public/js/management.js
@@ -146,6 +146,7 @@ async function updateUser(event)
     content.innerHTML = '<i class="fa-regular fa-snowflake fa-spin fa-4x vertical-center"></i>'
 
     let body = new FormData(event.target)
+    console.log(event.target);
     body.receives_alerts = event.target.receives_alerts.isSelected()
     let response = await fetch("/update_user", {
         headers: {
