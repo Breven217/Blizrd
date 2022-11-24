afcff9f7a68a9794c86b1b5215a68f990963829a
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-23 20:12:37 -0700

    Update management.js

--- public/js/management.js
+++ public/js/management.js
@@ -99,7 +99,7 @@ async function editUser(user_id=null)
         <input type="text" name="phone_number" id="phone-number" placeholder="Phone Number" value="` + user_data.phone_number + `">
         <input type="text" name="email_address" id="email-address" placeholder="Email Address" value="` + user_data.email_address + `">
         <label for="alert-box">Receives Alerts: </label>
-        <input type="checkbox" name="receives_alerts" id="alert-box" ` + user_data.receives_alerts ? 'checked>' + `>
+        <input type="checkbox" name="receives_alerts" id="alert-box" ` + user_data.receives_alerts ? 'checked>' : '' + `>
 
         <button type="button" name="delete_button" class="user-delete-button" onclick="deleteUser(`+user_id+`)">
             <span>
