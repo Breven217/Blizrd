38059443b72305d28fb595fa7671f0c7ac6edb92
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-23 18:46:41 -0700

    Update UserRequest.php

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -11,6 +11,14 @@ class UserRequest extends FormRequest
 {
     public ?User $user = null;
 
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
     protected function failedValidation(Validator $validator)
     {
         throw new HttpResponseException(response()->json(['errors' => $validator->errors()], 422));
