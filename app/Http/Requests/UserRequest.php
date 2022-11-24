c01a3efbca9f8b91e030b45542811f90ec8a3392
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-23 18:39:14 -0700

    Update UserRequest.php

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -4,10 +4,30 @@ namespace App\Http\Requests;
 
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
 
 class UserRequest extends FormRequest
 {
     public ?User $user = null;
+
+    /**
+     * Custom Failed Response
+     *
+     * Overrides the Illuminate\Foundation\Http\FormRequest
+     * response function to stop it from auto redirecting
+     * and applies a API custom response format.
+     *
+     * @param array $errors
+     * @return JsonResponse
+     */
+    public function response(array $errors) {
+
+        // Put whatever response you want here.
+        return new JsonResponse([
+            'status' => '422',
+            'errors' => $errors,
+        ], 422);
+    }
 
     /**
      * Get the validation rules that apply to the request.
