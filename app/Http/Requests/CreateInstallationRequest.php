9beb6f9e1bbb310fbdfea114247d185d944f175b
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-24 10:49:55 -0700

    Update CreateInstallationRequest.php

--- app/Http/Requests/CreateInstallationRequest.php
+++ app/Http/Requests/CreateInstallationRequest.php
@@ -17,7 +17,7 @@ class CreateInstallationRequest extends FormRequest
         return [
             'installed_on' => 'required|date',
             'location_id' => 'required|integer|exists:location,id',
-            'actions' => 'nullable|array:vehicle_id,user_id,installed',
+            'actions' => 'nullable',
                 'actions.*.vehicle_id' => 'required|integer|exists:vehicle,id',
                 'actions.*.user_id' => 'required|integer|exists:user,id',
                 'actions.*.installed' => 'required|boolean'
