ea0859780519b026fd9d60c485e930ea38435296
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-25 22:04:15 -0700

    Update reports.js

--- public/js/reports.js
+++ public/js/reports.js
@@ -1,11 +1,14 @@
 async function generateInstallationHistory() {
     let content = document.getElementById('report-content')
 
+    let firstDay = new Date(now.getFullYear(), now.getMonth(), 1)
+    let lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0)
+
     content.innerHTML = `
         <h2 class="report-title">Installation History</h2>
         <div class="report-dates">
-            <input type="date" id="report-date-to" name="report-date-to">
-            <input type="date" id="report-date-from" name="report-date-from">
+            <input type="date" id="report-date-to" name="report-date-to" value="`+firstDay+`">
+            <input type="date" id="report-date-from" name="report-date-from" value="`+lastDay+`">
         </div>
     `
 }
