af4eef437a07ad33e9176ad9db88f5e64037e8ab
Breven Joyner <104373751+Breven217@users.noreply.github.com>
2022-11-25 22:06:47 -0700

    Update reports.js

--- public/js/reports.js
+++ public/js/reports.js
@@ -1,11 +1,22 @@
+
+let currentReport = null
+//0 = Installation History
+
 async function generateInstallationHistory() {
+    generateTitle('Installation History')
+    currentReport = 0
+}
+
+function generateTitle(title=null)
+{
     let content = document.getElementById('report-content')
 
+    let now = new Date();
     let firstDay = new Date(now.getFullYear(), now.getMonth(), 1)
     let lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0)
 
     content.innerHTML = `
-        <h2 class="report-title">Installation History</h2>
+        <h2 class="report-title">`+title+`</h2>
         <div class="report-dates">
             <input type="date" id="report-date-to" name="report-date-to" value="`+firstDay+`">
             <input type="date" id="report-date-from" name="report-date-from" value="`+lastDay+`">
